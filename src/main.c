1d836222cec09721ae887bcf2f67e91b43f4e33b
Jakob Olsson <jakobolsson973@gmail.com>
2020-09-24 01:04:33 +0200

    Remade most functions

--- src/main.c
+++ src/main.c
@@ -11,30 +11,16 @@
 ////////////////// Physical definitions //////////////////////////////////
 #define MOTOR_LEFT    	OUTA
 #define MOTOR_FRONT   	OUTB
-#define MOTOR_C    		OUTC
 #define MOTOR_RIGHT    	OUTD
 #define SENSOR_TOUCH	IN1
 #define SENSOR_GYRO		IN2
-#define SENSOR_SCANER	IN3
-#define SENSOR_4		IN4
-#define MOTOR_BOTH     	( MOTOR_LEFT | MOTOR_RIGHT ) // Definition used to refer to both ports for the EV3:s wheel motors at the same time.
+#define SENSOR_SCANNER	IN3
+#define MOTOR_WHEELS    (MOTOR_LEFT|MOTOR_RIGHT) // Definition used to refer to both ports for the EV3:s wheel motors at the same time.
+#define MOTOR_ALL       (MOTOR_FRONT|MOTOR_LEFT|MOTOR_RIGHT)
 //////////////////////////////////////////////////////////////////////////
 
 ///////////////// Global variables ///////////////////////////////////////
-int maxSpeedWheels;         
-int maxSpeedCenter;
-// Touch
-POOL_T touchSensor; 
-int TouchReturnValue = 0;
-// Gyro
-POOL_T gyroSensor;
-int rotationReference = 0;
-int degrees;
-// Sonic
-POOL_T ultraSonic;
-int sonicSensor;
-// scaner
-POOL_T scaner;
+
 //////////////////////////////////////////////////////////////////////////
 
 /////////////// Function Declarations ////////////////////////////////////
@@ -56,28 +42,23 @@ void findWall(int gyro);
 ////////////////////// Main //////////////////////////////////////////////
 int main()
 {
-    if(!brick_init())
-	{
-		return (1);
-	}
-	rotationReference = motorSensorInit();
+    initEverything();
 
+	moveForward(10);
+	moveBackward(10);
+	rotate(360);
+	releaseBook();
 
-
-    brick_uninit();
-	printf( "dying...\n" );
-    return ( 0 );
+    unInitEverything();
 }
 //////////////////////////////////////////////////////////////////////////
 
 //////////////////////// Initialization //////////////////////////////////
 void initEverything()
 {
-    if (!brick_init())  // instantiates brick and allows remote-connection
-	{
-		return (1);
-	}
+    brickInit();
     motorsInit();
+    initSensors();
 }
 
 void unInitEverything()
@@ -85,22 +66,20 @@ void unInitEverything()
     brick_uninit();
 }
 
+void brickInit()
+{
+    if (!brick_init())  // instantiates brick and allows remote-connection
+	{
+		return (1);
+	}
+}
+
 void motorsInit()
 {
-    if (tacho_is_plugged(MOTOR_BOTH, TACHO_TYPE__NONE_))    // Checks if motors are connected to the "bricks" ports that are supposed to be used
+    if (tacho_is_plugged(MOTOR_ALL, TACHO_TYPE__NONE_))    // Checks if motors are connected to the "bricks" ports that are supposed to be used
     {  
-        maxSpeedCenter = tacho_get_max_speed(MOTOR_FRONT, 0);   // Checks the max speed of the small motor and assigns it to global variable
-        if (tacho_get_max_speed(MOTOR_LEFT, 0) < tacho_get_max_speed(MOTOR_RIGHT, 0))
-        {
-            maxSpeedWheels = tacho_get_max_speed(MOTOR_LEFT, 0); 
-        }
-        else
-        {
-            maxSpeedWheels = tacho_get_max_speed(MOTOR_RIGHT, 0); 
-        }
-        
-        tacho_reset(MOTOR_BOTH);
-        printf("Motors Initialized\n");
+        tacho_reset(MOTOR_ALL);
+        printf("Tachos Initialized\n");
     }
     else 
     {
@@ -113,132 +92,64 @@ void motorsInit()
     }
 }
 
-int motorSensorInit(){
-	
-	if ( !brick_init()) return ( 1 ); /*Instansierar klossen och tillåter remote-connection*/
-	printf( "*** ( EV3 ) Hello! ***\n" );
-	Sleep(2000);
-
-	if(!sensor_is_plugged((SENSOR_TOUCH|SENSOR_GYRO), SENSOR_TYPE__NONE_)) {
+void initSensors()
+{
+    if(!sensor_is_plugged((SENSOR_TOUCH|SENSOR_GYRO), SENSOR_TYPE__NONE_)) {
 		printf("Stoppa in sensorer i port 1 och 2\n");
 		brick_uninit();
 		return(0);
 	}
-	/*Registrerar en sensor på sensor variabeln*/
-	touchSensor = sensor_search(LEGO_EV3_TOUCH);
-	gyroSensor = sensor_search(LEGO_EV3_GYRO);
-	ultraSonic = sensor_search(LEGO_EV3_US); // Kolla med gustav
-	
-	//registerar sensor mode
-	sensor_set_mode(gyroSensor, LEGO_EV3_GYRO_GYRO_G_AND_A);
-	touch_set_mode_touch(touchSensor);
-		
-	rotationReference = sensor_get_value(0, gyroSensor, 0);// hämtar värde
 
-	if ( tacho_is_plugged( MOTOR_BOTH, TACHO_TYPE__NONE_ )) {  /* TACHO_TYPE__NONE_ = Alla typer av motorer */
-        maxSpeedWheels = tacho_get_max_speed( MOTOR_LEFT, 0 );	/*Kollar max hastigheten som motorn kan ha*/
-        maxSpeedCenter = tacho_get_max_speed( MOTOR_FRONT, 0 );
-		tacho_reset( MOTOR_BOTH );
-		tacho_reset( MOTOR_FRONT );
-    } else {
-        printf( "Stoppa in vänstra motorn i port D,\n"
-        "Stoppa in högra motorn i port A.\n"
-        );
-      	brick_uninit();
-        return ( 0 );  /* Stänger av sig om motorer ej är inkopplade */
-    }
-	
-	if ( tacho_is_plugged( MOTOR_FRONT, TACHO_TYPE__NONE_ )) {  
-        maxSpeedCenter = tacho_get_max_speed( MOTOR_FRONT, 0 );	
-        tacho_reset( MOTOR_FRONT );
-    } else {
-        printf( "Stoppa in vänstra motorn i port A,\n"
-        "Stoppa in högra motorn i port B.\n"
-        );
-      	brick_uninit();
-        return ( 0 );  /* Stänger av sig om motorer ej är inkopplade */
-    }
-	return rotationReference;
+    sensor_set_mode(SENSOR_GYRO, LEGO_EV3_GYRO_GYRO_G_AND_A);
+	touch_set_mode_touch(SENSOR_TOUCH);
 }
 //////////////////////////////////////////////////////////////////////////
 
 ////////////////// Movement //////////////////////////////////////////////
 void moveForward(int distance)
 {
-    // Run motor 1 and 2 forwards at the same speed
-    // Keep running until the "distance" has been met
-
-    tacho_set_speed_sp(MOTOR_BOTH, maxSpeedWheels * 0.2);
-    tacho_set_position_sp(MOTOR_BOTH, distance*(360/17));
-    tacho_run_to_rel_pos(MOTOR_BOTH);
-    tacho_stop(MOTOR_BOTH);
+    tacho_set_speed_sp(MOTOR_WHEELS, maxSpeedWheels * 0.2);
+    tacho_set_position_sp(MOTOR_WHEELS, distance*(360/17));
+    tacho_run_to_rel_pos(MOTOR_WHEELS);
+    tacho_stop(MOTOR_WHEELS);
 }
 
 void moveBackward(int distance)
 {
-    // Run motor 1 and 2 backwards at the same speed
-    // Keep running until the "distance" has been met
-
-    tacho_set_speed_sp(MOTOR_BOTH, -(maxSpeedWheels * 0.2));
-    tacho_set_position_sp(MOTOR_BOTH, distance*(360/17));
-    tacho_run_to_rel_pos(MOTOR_BOTH);
-    tacho_stop(MOTOR_BOTH);
+    tacho_set_speed_sp(MOTOR_WHEELS, -(maxSpeedWheels * 0.2));
+    tacho_set_position_sp(MOTOR_WHEELS, distance*(360/17));
+    tacho_run_to_rel_pos(MOTOR_WHEELS);
+    tacho_stop(MOTOR_WHEELS);
 }
 
-void moveUltraSonic(int wallDistance)
+void movePerpendicularToWall(int desiredDistanceFromWall)
 {
-	// variabel definitioner
-	sonicSensor= sensor_get_value(0,ultraSonic,0);
-	int difference;
-	
-	while(sonicSensor==wallDistance)
-	{
-		// beraknar rorelse avstand 
-		difference= sonicSensor - wallDistance;
-		
-		if (difference >= 0)
-		{
-			// langre bort fran vaggen
-			moveForward(difference);	
-		}
-		else
-		{
-			// narmare till vaggen
-			moveBackward(difference*(-1));
-		}
-		// updaterar avstand
-		sonicSensor= sensor_get_value(0,ultraSonic,0);
-	}
-	return;
+	int distanceToMove = sensor_get_value(0,SENSOR_SCANNER,0) - desiredDistanceFromWall;
+
+    while(!(desiredDistanceFromWall == sensor_get_value(0,SENSOR_SCANNER,0)))
+    {
+        if (distanceToMove > 0)
+        {
+            moveForward(distanceToMove);
+        }
+        else
+        {
+            moveBackward(distanceToMove);
+        } 
+    }
 }
 
-int rotate(int rotationReference, int degrees)
+void rotate(int degrees)
 {
-	// svänga höger eller vänster
-	if (degrees>0)
+	int rotationTarget = sensor_get_value(0,SENSOR_GYRO,0) + degrees;
+
+	while(!(sensor_get_value(0,SENSOR_GYRO,0) == rotationTarget)
 	{
-		// startar motoren
-		tacho_set_speed_sp(MOTOR_RIGHT, maxSpeedWheels * -0.2);
-		tacho_run_forever(MOTOR_RIGHT);
-		// kontrollerar vinkel
-		while(rotationReference <= degrees)
-		{
-			rotationReference = sensor_get_value(0,gyroSensor,0);
-		}
-		tacho_stop(MOTOR_RIGHT);
+		tacho_set_speed_sp(MOTOR_RIGHT, -200);
+		tacho_set_speed_sp(MOTOR_LEFT, 200);
+		tacho_run_forever(MOTOR_WHEELS);
 	}
-	else
-	{
-		// startar motoren
-		tacho_set_speed_sp(MOTOR_LEFT, maxSpeedWheels * 0.2);
-		tacho_run_forever(MOTOR_LEFT);
-		// kontrollerar vinkel
-		while(rotationReference >= degrees)
-		{
-			rotationReference = sensor_get_value(0,gyroSensor,0);
-		}
-		tacho_stop(MOTOR_LEFT);	
-	}
+	tacho_stop(MOTOR_WHEELS);
 }
 //////////////////////////////////////////////////////////////////////////
 
