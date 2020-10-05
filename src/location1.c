87255f351409101834c95a75790fe9195c64e306
Jakob Olsson <jakobolsson973@gmail.com>
2020-10-05 12:01:29 +0200

    New align paralell functions in all programs

--- src/location1.c
+++ src/location1.c
@@ -9,12 +9,13 @@
 //////////////////////////////////////////////////////////////////////////
 
 ////////////////// Physical definitions //////////////////////////////////
-#define MOTOR_LEFT    	OUTA
-#define MOTOR_FRONT   	OUTB
-#define MOTOR_RIGHT    	OUTD
-#define SENSOR_TOUCH	IN1
-#define SENSOR_GYRO		IN2
-#define SENSOR_SCANNER	IN3
+#define MOTOR_LEFT    	    OUTA
+#define MOTOR_FRONT   	    OUTB
+#define MOTOR_RIGHT    	    OUTD
+#define SENSOR_TOUCH_LEFT	IN1
+#define SENSOR_GYRO		    IN2
+#define SENSOR_SCANNER	    IN3
+#define SENSOR_TOUCH_RIGHT  IN4
 #define MOTOR_BOTH      (MOTOR_LEFT|MOTOR_RIGHT) // Definition used to refer to both ports for the EV3:s wheel motors at the same time.
 #define MOTOR_ALL       (MOTOR_FRONT|MOTOR_LEFT|MOTOR_RIGHT)
 //////////////////////////////////////////////////////////////////////////
@@ -46,13 +47,13 @@ int main()
     initEverything();
 
     alignParallelWithWall(-1);
-    moveForward(150);
+    moveForward(250);
     rotate(90);
     moveTowardsWallAndStop(30);
     releaseBook();
     moveBackward(20);
     rotate(90);
-    moveForward(150);
+    moveForward(250);
 
     unInitEverything();
 }
@@ -106,7 +107,7 @@ void moveForward(int distance)
     int targetPosition = 1.05*(tacho_get_position(MOTOR_LEFT, targetPosition) + distance*20);
     tacho_set_speed_sp(MOTOR_BOTH, 200);
 
-    while(true)
+    while(true) 
     {
         if(targetPosition > tacho_get_position(MOTOR_LEFT, targetPosition))
         {
@@ -217,23 +218,25 @@ void releaseBook()
 void alignParallelWithWall(int side)
 {
     int targetRotation = findWall(360);
-    rotateAbsolute(targetRotation - 90, 200);
+    rotateAbsolute(targetRotation + 180, 200);
 
-    targetRotation = 0;
-    int amountOfScans = 2;
-    for(int i = 1; i <= amountOfScans; i++)
+    tacho_set_speed_sp(MOTOR_BOTH, -200);
+    tacho_run_forever(MOTOR_BOTH);
+    bool running = true;
+    while(running)
     {
-        if(i % 2 == 0)
+        if(sensor_get_value(0,SENSOR_TOUCH_LEFT,0) && sensor_get_value(0,SENSOR_TOUCH_RIGHT,0))
         {
-            targetRotation = targetRotation + (findWall(-180) / amountOfScans);
-        }
-        else
-        {
-            targetRotation = targetRotation + (findWall(180) / amountOfScans);
+            Sleep(3000);
+            tacho_stop(MOTOR_BOTH);
+            targetRotation = sensor_get_value(0,SENSOR_GYRO,0);
+            running = false;
         }
     }
 
-    rotateAbsolute(targetRotation + (90*side), 100);
+    moveForward(50);
+
+    rotateAbsolute(targetRotation - (90*side), 100);
     Sleep(500);
 }
 
